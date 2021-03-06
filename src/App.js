b427e2a2f8dcb7ad41b4cb762817da4202693fb9
Dtkrontz <dtkrontz@gmail.com>
2021-03-06 15:59:02 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import SiteBar from './Home/Navbar';
 import Auth from './Auth/Auth';
+import WorkoutIndex from './Workouts/WorkoutIndex';
 
 function App() {
-
   const [sessionToken, setSessionToken] = useState(''); //1
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -22,10 +27,14 @@ function App() {
     document.title = 'Workout Log Client'
   }, [])
 
+  const protectedViews = () => {
+    return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken} /> : <Auth updateToken={updateToken} />)
+  }
+
   return (
     <div>
-      <SiteBar />
-      <Auth updateToken={updateToken} />
+      <SiteBar clearToken={clearToken} />
+      {protectedViews()}
     </div>
   );
 }
--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -3,20 +3,35 @@ import {Form, FormGroup, Label, Input, Button} from 'reactstrap'; //1
 
 
 const Login = (props) => {
-    const [username, setUserName] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('http://localhost:4000/user/login', {
+            method: 'POST',
+            body: JSON.stringify({user: {username: username, password: password}}),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            props.updateToken(data.sessionToken);
+        })
+    }
 
     return (
         <div>
             <h1>Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <FormGroup>
                     <Label htmlFor='username'>Username</Label>
-                    <Input name='username' value={username} />
+                    <Input onChange={(e) => setUsername(e.target.value)} name='username' value={username} />
                 </FormGroup>
                 <FormGroup>
                     <Label htmlFor='password'>Password</Label>
-                    <Input name='password' value={password} />
+                    <Input onChange={(e) => setPassword(e.target.value)} name='password' value={password} />
                 </FormGroup>
             </Form>
         </div>
